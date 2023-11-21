f95ba256172ec1cf1591c2b4927708c151fcc55f
rGabrielLima <renangblima@gmail.com>
2023-11-20 22:28:35 -0300

    first commit

--- processamento_de_envio.php
+++ processamento_de_envio.php
@@ -62,8 +62,8 @@
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'renangblima@gmail.com';                     //SMTP username
-			$mail->Password   = 'hqkl xzgw engx qadd';                               //SMTP password
+			$mail->Username   = 'seu email';                     //SMTP username
+			$mail->Password   = 'sua senha';                               //SMTP password
 			$mail->SMTPSecure = "tls";         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
---
+++ readme.md.txt
@@ -0,0 +1,1 @@
+Projeto bem hardcode, ainda aprendendo.
